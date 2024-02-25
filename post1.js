d7e173e1280ba11eeedcf83862c2de20cab9f6be
Dante Bertuzzi <67659136+dantebertuzzi@users.noreply.github.com>
2024-02-25 16:37:15 -0300

    Add files via upload
    page 01

---
+++ post1.js
@@ -0,0 +1,79 @@
+// Função para copiar o conteúdo da div com a classe "code-container"
+function copyCode(element) {
+    // Obtém o código dentro da div clicada
+    var codeContent = element.parentNode.querySelector('code').innerText;
+
+    // Cria um elemento de textarea temporário para armazenar o código
+    var tempTextarea = document.createElement('textarea');
+    tempTextarea.value = codeContent;
+
+    // Adiciona o textarea à página
+    document.body.appendChild(tempTextarea);
+
+    // Seleciona e copia o conteúdo do textarea
+    tempTextarea.select();
+    document.execCommand('copy');
+
+    // Remove o textarea temporário
+    document.body.removeChild(tempTextarea);
+
+    // Notifica o usuário que o código foi copiado (pode ser personalizado)
+    alert('Código copiado!');
+}
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
